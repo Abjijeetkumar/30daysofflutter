1d16d7fa354587a2b936a33997561bd00fce0c1a
unknown <softech.abk827013@gmail.com>
2021-07-29 11:58:47 +0530

    Day3 complete

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   
-
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData( primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(brightness: Brightness.dark,
-      
-    ),
-    initialRoute: '/home',
-    routes: {
-      '/' : (context) => LoginPage(),
-      '/home' : (context) => HomePage(),
-      '/Login' : (context) => LoginPage(),
-    },
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: '/home',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/Login': (context) => LoginPage(),
+      },
     );
   }
 }
